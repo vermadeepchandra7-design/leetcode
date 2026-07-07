810eb5f45826be3f150c0275beca478b17ce5353
vermadeepchandra7-design <vermadeepchandra7@gmail.com>
2026-07-07 13:46:24 +0530

    Time: 0 ms (100%), Space: 7.7 MB (90.81%) - LeetHub

--- 1952-three-divisors/1952-three-divisors.cpp
+++ 1952-three-divisors/1952-three-divisors.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool isThree(int n) {
-        int count=0;
-        for(int i=1; i<=n; i++)
-        {
-            if(n%i==0)
-            {
-                count++;
+
+        if( n == 0){
+            return 0 ;
+        }
+        int count = 0 ;
+        for(int i = 1 ; i <= n ; i ++){
+            if(n % i == 0){
+                count ++ ;
             }
         }
-        if(count==3)
-        {
-            return true;
+        if(count == 3){
+            return 1 ; 
         }
-        else
-        {
-            return false;
-        }
+        
+        return 0 ;
     }
-};
+};  /*   Time complexity  : O(n) 
+        space complexity  : O(1)      */
