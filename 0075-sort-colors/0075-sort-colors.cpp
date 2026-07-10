10ab375c17e457baea1163edc08689c44de427fa
Deepchandra Verma <vermadeepchandra7@gmail.com>
2026-07-10 13:51:52 +0530

    Time: 0 ms (100%), Space: 11.6 MB (49.09%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,45 +1,31 @@
 class Solution {
 public:
-/*  Approach : Traverse the given vector and collect the count of 0 , 1 and 2 and than as per the  respective count value overwrite 0 , 1 and 2 in the given vector*/
+
     void sortColors(vector<int>& nums) {
+        
+        /* : Optimized solution :  */
+        int n = nums.size() ;
+        int i = 0 ;     // for handling 0
+        int j = 0 ;    // for handling 1
+        int k = n-1 ; // for handling 2
 
-        // solved by the better approach
-        int n = nums.size();
-        int zero_count = 0;
-        int one_count = 0;
-        int two_count = 0;
+        while( j <= k ){
 
-        for (int i = 0; i < n; i++) {
-            // traversing vector for collecting the count of 0 , 1 , 2
-            if (nums[i] == 0) {
-                zero_count++;
-            } else if (nums[i] == 1) {
-                one_count++;
-            } else {
-                two_count++;
+            if(nums[j] == 1){
+                j ++ ;
+            }
+            else if(nums[j] == 2){
+                swap(nums[j] , nums[k]) ;
+                k -- ;
+            }
+            else{
+                // nums[j] == 0
+                swap(nums[j] , nums[i]) ;
+                    i ++ ;
+                    j ++ ;
+                }
             }
         }
-        int i = 0 ;
-            while ( i < n) {
-                // now putting 0 , 1 , 2 as per their respective count received
-                while (zero_count) {
-                     nums[i] = 0 ;
-                     i++ ;
-                    zero_count --;
-                }
-                while (one_count) {
-                    nums[i] = 1 ;
-                    i++ ;
-                    one_count -- ;
-                }
-                while (two_count) {
-                    nums[i] = 2 ;
-                    i ++ ;
-                    two_count -- ;
-                }
-            }
-        
-        return;
-    } 
-};     /*   Time complexity  :O(n)
-            space complexity :O(1)    */ 
+    
+};     /*      Time complexity    : O(n)  
+               space complexity   : O(1)*/
