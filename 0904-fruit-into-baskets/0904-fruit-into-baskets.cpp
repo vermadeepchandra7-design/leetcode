1ae2d4ce2692b25e14225d68a7e1684861ff66e2
Deepchandra Verma <vermadeepchandra7@gmail.com>
2026-07-15 16:05:42 +0530

    Time: 32 ms (85.68%), Space: 84.8 MB (62.61%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -8,32 +8,35 @@ public:
 4. Puri array traverse hone tak process repeat karo.
 */
     int totalFruit(vector<int>& fruits) {
-          // BY : Sliding Window 
-         int n = fruits.size() ;
-         // Variable size Window 
-         int low = 0 , high = 0 ; 
-         int result = 0 ;
-         unordered_map<int , int>mp;
+        // By : sliding window
+        int n = fruits.size() ;
+        int result = 0 ;
+        // variable size window ( Mike solution )
+        int low = 0 , high = 0 ;
+        unordered_map<int , int>mp ;
 
-         while(high < n){
-            // including high in reqired information 
+        while(high < n){
+            // including high in the current window 
             mp[fruits[high]]++ ;
-            while(mp.size() > 2 ){
-
-                mp[fruits[low]]--;
-                if( mp[fruits[low]] == 0){
-                    mp.erase(fruits[low]) ;
-                }
-                low++ ;
-            }
             if(mp.size() <= 2){
-
-                int len = (high - low)+1 ;
+                int len = (high - low) + 1 ;
                 result = max(result , len) ;
             }
+            else{
+                /* mp.size() > 2 means we have wrong window size which is not    required so discard low from the current window*/
+                mp[fruits[low]]-- ;
+                if(mp[fruits[low]] == 0){
+                    mp.erase(fruits[low]) ;
+                }
+               // after discarding low from the current window incresase the low pointer 
+                low ++ ;
+
+                
+            }
             high ++ ;
-         }
+        }
         return result ;
     }
-};  /*    Time complexity  : O(n) 
-          space complexity : O(1)  */
+};  /*     Time complexity  : O(n)  
+           space complexity : O(k) 
+                            where k = size of unorderd map */
