495f1f8c2eeffe9a1286e2a7b44f3e56285db053
Deepchandra Verma <vermadeepchandra7@gmail.com>
2026-07-16 15:30:52 +0530

    Time: 17 ms (7.41%), Space: 141.4 MB (13.38%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-     // Brute force solution 
-     int n = nums.size() ;
-     vector<int>v1 ;
-     vector<int>v2 ;
-     for(int i = 0 ; i < n ; i++){
-    // T.C = O(n)
-        if(nums[i] < pivot){
-            v1.push_back(nums[i]) ;
+        // Brute force solution 
+        int n = nums.size() ;
+        vector<int>less_pivot ;
+        vector<int>equal_pivot ;
+        vector<int>greater_pivot ;
+        int i = 0 ;
+        while(i < n){
+            if(nums[i] == pivot){
+                equal_pivot.push_back(nums[i]) ;
+            }
+            else if(nums[i] < pivot){
+                less_pivot.push_back(nums[i]) ;
+            }
+            else{
+                greater_pivot.push_back(nums[i]);
+            }
+            i++ ;
         }
-        else if(nums[i] > pivot){
-            v2.push_back(nums[i]) ;
+        // Merging all the vectors togather
+        for(int & num : equal_pivot){
+            less_pivot.push_back(num);
         }
-     }
-     // puting elements equals to pivot in the vector v1 
-     for(int i = 0 ; i < n ; i++){
-        // T.C = O(n)
-        if(nums[i] == pivot){
-            v1.push_back(nums[i]);
+        for(int & num : greater_pivot){
+            less_pivot.push_back(num);
         }
-     }
-      
-     // Merging v1 and v2
-     v1.insert(v1.end() , v2.begin() , v2.end()) ;
-     // T.C = O(n) : for merge operation
-
-     return v1 ;
+        return less_pivot ;
     }
-};  /*  Time complexity  : O(n) 
-        space complexity : O(n)  */
+};  /*  Time  Complexity : O(n) 
+        space Complexity : O(n)   */
