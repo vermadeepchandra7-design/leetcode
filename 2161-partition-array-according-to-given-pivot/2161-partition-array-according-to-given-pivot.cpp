ac8da4e1744cc9446f514dbfbf1269bc143dccd8
Deepchandra Verma <vermadeepchandra7@gmail.com>
2026-07-16 15:57:43 +0530

    Time: 7 ms (58.8%), Space: 127.7 MB (88.34%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -1,32 +1,41 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        // Brute force solution 
+        // Better Approach 
         int n = nums.size() ;
-        vector<int>less_pivot ;
-        vector<int>equal_pivot ;
-        vector<int>greater_pivot ;
-        int i = 0 ;
-        while(i < n){
-            if(nums[i] == pivot){
-                equal_pivot.push_back(nums[i]) ;
+        int count_less = 0 ;
+        int count_equal = 0 ;
+        for(int & num  : nums){
+            // for getting count of elements in the given vector 
+            if( num < pivot){
+                count_less ++ ;
             }
-            else if(nums[i] < pivot){
-                less_pivot.push_back(nums[i]) ;
+            else if(num == pivot){
+                count_equal ++ ;
+            }
+        }
+        int i = 0 ; // handle elements less than pivot 
+        int j = count_less ; // handle elments equal to pivot
+        int k = count_less + count_equal ; // handle elements greater than pivot
+        // creating resultant vector 
+        vector<int>result(n) ;
+        for(int & num : nums){
+            // now storing all the values in the resultant
+            if(num < pivot){
+                result[i] = num ;
+                i++ ;
+            }
+            else if(num == pivot){
+                result[j] = num ;
+                j++ ;
             }
             else{
-                greater_pivot.push_back(nums[i]);
+                result[k] = num ;
+                k++ ;
             }
-            i++ ;
         }
-        // Merging all the vectors togather
-        for(int & num : equal_pivot){
-            less_pivot.push_back(num);
-        }
-        for(int & num : greater_pivot){
-            less_pivot.push_back(num);
-        }
-        return less_pivot ;
+
+        return result ;
     }
-};  /*  Time  Complexity : O(n) 
-        space Complexity : O(n)   */
+}; /*  Time complexity  : O(n) 
+       space complexity : O(n) */
