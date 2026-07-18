bb51c443ec36e6e8b6cf83944974772d895dc4ff
Deepchandra Verma <vermadeepchandra7@gmail.com>
2026-07-18 22:13:42 +0530

    Time: 0 ms (100%), Space: 15.8 MB (8.64%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -7,4 +7,9 @@ public:
      /* By next_permutation() function in STL */ 
      next_permutation(nums.begin() , nums.end());
     }
-};
+}; /* Time Complexity : O(n)
+      Reason: next_permutation() scans the array and reverses a part of it once.
+
+      Space Complexity : O(1)
+      Reason: It modifies the array in-place without using any extra space.
+*/
