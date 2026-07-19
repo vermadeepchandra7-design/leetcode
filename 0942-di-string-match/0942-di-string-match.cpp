ce6a62d4df8c151519763d61b5a1f3084f35bbaf
Deepchandra Verma <vermadeepchandra7@gmail.com>
2026-07-19 15:17:14 +0530

    Time: 3 ms (16.43%), Space: 11.7 MB (78.01%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+
+        // Approach:
+        // 1. Initialize two pointers: low = 0 and high = n.
+        // 2. Traverse the string using a while loop.
+        // 3. If current character is 'I', place low and increment low.
+        // 4. If current character is 'D', place high and decrement high.
+        // 5. Append the last remaining number.
+
+        int n = s.size();
+
+        vector<int> ans;
+
+        // Two pointers
+        int low = 0, high = n;
+        int i = 0;
+
+        while(i < n) {
+
+            if(s[i] == 'I') {
+                ans.push_back(low);
+                low++;
+            }
+            else {
+                ans.push_back(high);
+                high--;
+            }
+
+            i++;
+        }
+
+        // Append the last remaining number
+        ans.push_back(low);
+
+        return ans;
+    }
+};
+
+/*
+Time Complexity: O(n)
+
+Space Complexity: O(n)
+*/
