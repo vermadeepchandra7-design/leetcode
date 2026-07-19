2bd6a47c2ba57d8629aa75e444c962ecb8d99142
Deepchandra Verma <vermadeepchandra7@gmail.com>
2026-07-19 13:29:47 +0530

    Time: 0 ms (100%), Space: 15.9 MB (8.83%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,15 +1,41 @@
 class Solution {
 public:
+/* Approach:
+ 1. Find the pivot (first element from the right where nums[i] > nums[i-1]).
+ 2. If no pivot exists, reverse the entire array.
+ 3. Find the first element greater than the pivot from the right.
+ 4. Swap the pivot with that element.
+ 5. Reverse the suffix to get the next permutation.
+   */
+
     void nextPermutation(vector<int>& nums) {
+        
+        int n = nums.size() ;
+        int pivot_idx = -1 ;
 
-      /*  Brute force : */
-      int n = nums.size() ;
-     /* By next_permutation() function in STL */ 
-     next_permutation(nums.begin() , nums.end());
+        for(int i = n-1 ; i > 0 ; i--){
+            // Finding the index of pivot element 
+            if(nums[i] > nums[i-1]){
+                pivot_idx = i-1 ;
+                break ;
+              }
+                 
+            }
+        if(pivot_idx != -1){
+            int swap_idx ;
+            for(int j = n-1 ; j > pivot_idx + 1 ; j--){
+                if(nums[j] > nums[pivot_idx]){
+                    swap_idx = j ;
+                    break ;
+                }
+                
+            }
+            swap(nums[pivot_idx] , nums[swap_idx]) ;
+            reverse(nums.begin()+pivot_idx+1 , nums.end());
+         }
+         else{
+            reverse(nums.begin() , nums.end()) ;
+         }
     }
-}; /* Time Complexity : O(n)
-      Reason: next_permutation() scans the array and reverses a part of it once.
-
-      Space Complexity : O(1)
-      Reason: It modifies the array in-place without using any extra space.
-*/
+};  /*    Time complexity  : O(n) 
+          space complexity : O(1)    */
