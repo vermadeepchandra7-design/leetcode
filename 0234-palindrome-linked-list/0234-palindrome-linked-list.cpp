5a64e733d680dcab16b1327b4aeeb62b22191038
Deepchandra Verma <vermadeepchandra7@gmail.com>
2026-08-25 22:45:57 +0530

    Time: 0 ms (100%), Space: 131.9 MB (16.61%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,16 +10,16 @@
  */
 class Solution {
 public:
-/*  Appraoch : (Brute force)
-1. Create a pointer to traverse the linked list.
-2. Store all node values in a vector.
-3. Find the size of the vector.
-4. Initialize two pointers: i at start and j at end.
-5. Compare the values at i and j.
-6. If values differ, return false.
-7. Move i forward and j backward.
-8. If all values match, return true.
-*/
+    /*  Appraoch : (Brute force)
+    1. Create a pointer to traverse the linked list.
+    2. Store all node values in a vector.
+    3. Find the size of the vector.
+    4. Initialize two pointers: i at start and j at end.
+    5. Compare the values at i and j.
+    6. If values differ, return false.
+    7. Move i forward and j backward.
+    8. If all values match, return true.
+    */
 
     bool isPalindrome(ListNode* head) {
 
