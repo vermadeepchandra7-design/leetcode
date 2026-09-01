59e8378922d590fc92f1ddf6d25084a3189b5470
Deepchandra Verma <vermadeepchandra7@gmail.com>
2026-09-01 23:00:15 +0530

    Time: 3 ms (65.91%), Space: 12.2 MB (49.17%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+
+        vector<int> val{
+            1000, 900, 500, 400, 100, 90,
+            50, 40, 10, 9, 5, 4, 1
+        };
+
+        vector<string> sym{
+            "M", "CM", "D", "CD", "C", "XC",
+            "L", "XL", "X", "IX", "V", "IV", "I"
+        };
+
+        string result = "";
+
+        for (int i = 0; i < 13; i++) {
+
+            if (num == 0)
+                break;
+
+            int times = num / val[i];
+
+            while (times--) {
+                result += sym[i];
+            }
+
+            num = num % val[i];
+        }
+
+        return result;
+    }
+}; /*   Time Complexity  : O(1)
+        Space Complexity : O(1)  */
